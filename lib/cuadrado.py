380e7bab5a8a73978d54daea466bed246d8558df
Jorge Gonzalez Sanchez <jorgepazosglez@gmail.com>
2025-09-23 19:21:32 +0200

    Mi primer commit

---
+++ lib/cuadrado.py
@@ -0,0 +1,3 @@
+def area_cuadrado(l:int)->int:
+	return l**2
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from lib import cuadrado
+print("Proyecto Cuadrado")
+l=4
+print(f"El area del cuadrado de lado {l} es: {cuadrado.area_cuadrado(l)}")
