e34a098fd3b72f65437837bcf7b38ecc38aa8815
Jorge Gonzalez Sanchez <jorgepazosglez@gmail.com>
2025-10-07 18:07:09 +0200

    Agregadas las funcionalidades para el triangulo

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from lib import cuadrado
+from lib import cuadrado, triangulo
 print("Proyecto Cuadrado")
 print(cuadrado.get_identificador())
 l=4
 print(f"El area de un {cuadrado.get_identificador()} de lado {l} es:{cuadrado.area_cuadrado(l)} y el perimetro es {cuadrado.get_perimetro(l)}")
+ 
+base=4
+altura=2
+print(triangulo.get_identificador())
+print(f"El area de {triangulo.get_identificador())} de base {base} y altura {altura}\
+	es: {triangulo.get_area(base,altura)} y el perimetro es: {triangulo.get_perimetro(base,base,base)}")
